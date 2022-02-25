3dc13da4611de1a4f0b889e5ad43657920f253c6
Arunkumar D <99382863+arunkumar021@users.noreply.github.com>
2022-02-25 01:38:10 -0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,10 @@
 <template>
-  <button @click="activeTab = 'StoryBook'">Page-1</button>
-  <button @click="activeTab = 'PageTwo'">Page-2</button>
-  <button @click="activeTab = 'PageThree'">Page-3</button>
-  <StoryBook v-if="activeTab === 'StoryBook'"/>
-  <PageTwo v-if="activeTab === 'PageTwo'"/>
-  <PageThree v-if="activeTab === 'PageThree'"/>
+  <StoryBook/>
 </template>
 
 <script>
 
 import StoryBook from './components/StoryBook.vue'
-import PageThree from './components/PageThree.vue'
-import PageTwo from './components/PageTwo.vue'
 export default {
   name: 'App',
   data() {
@@ -21,8 +14,7 @@ export default {
   },
   components: {
     StoryBook,
-    PageTwo,
-    PageThree,
+
   }
 }
 </script>
